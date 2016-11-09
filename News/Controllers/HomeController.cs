c215d80bcb1be39fe43a2c3c87d5fccb38e176fa
1351300127 <1351300127@qq.com>
2016-11-09 11:30:14 +0800

    blogarticle添加验证修饰，添加博客文章页面使用模型验证

--- News/Controllers/HomeController.cs
+++ News/Controllers/HomeController.cs
@@ -20,6 +20,10 @@ namespace News.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 欢迎页面 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Welcome()
         {
             var a = 100;
@@ -33,10 +37,50 @@ namespace News.Controllers
             return View();
         }
 
+
+        /// <summary>
+        /// 新闻列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List(int page=10)
+        {
+            string[] data= new string[] { "台风逼近广西景区关闭学校停课",
+                "广西每年入境游客超300万" ,
+                "空军招飞初选10月24日开始",
+                "柳州城市轨道交通勘探工作已展开"};
+
+            ViewBag.data = data;
+            ViewBag.Page = page;
+
+            //ViewData["data"] = data;
+            //ViewData.Model = data;
+            return View();
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        /// <summary>
+        /// 添加新闻
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 保存新闻内容
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Save(string title, string content)
+        {
+            ViewBag.Title = title;
+            ViewBag.Content = content;
             return View();
         }
     }
